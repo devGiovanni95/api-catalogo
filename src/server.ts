8d00f002617d4ee1499ffad4eb58a019e32f6380
devGiovanni95 <giovanni.devjava@hotmail.com>
2023-09-09 12:55:33 -0300

    0.1.1-Config server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log("Api is running")
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+const app = express()
+const port = process.env.PORT
+
+app.listen(port, ()=> {
+    console.log("Api is running")
+})
