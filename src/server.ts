1f9567f19f9868b8037d7aa71559f7a6b5dfc61e
Giovanni Almeida dos Santos <69327898+devGiovanni95@users.noreply.github.com>
2023-09-10 01:31:53 -0300

    0.8.0-Implemented address in the cors on server.ts

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,9 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT
 
-app.use(cors())
+app.use(cors({
+    origin:['http://localhost:3000','https://api-catalogo-h0fl.onrender.com/']
+}))
 
 app.use(express.json())
 app.use(routes)
